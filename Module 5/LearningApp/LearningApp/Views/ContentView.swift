c22efa9d275b0e457df0eb7c56640b25d43dd853
Sabit Ahmed <sabit.a.sirat@gmail.com>
2021-09-30 22:09:00 +0600

    add HomeViewRow and ContentView

---
+++ Module 5/LearningApp/LearningApp/Views/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Sabit Ahmed on 30/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Module 5/LearningApp/LearningApp/Views/HomeView.swift
+++ Module 5/LearningApp/LearningApp/Views/HomeView.swift
@@ -11,13 +11,44 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            VStack (alignment: .leading) {
+                Text("What do you want to learn today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    LazyVStack {
+                        ForEach(model.modules) { module in
+                            
+                            VStack (spacing: 20) {
+                                // MARK: Learning Card
+                                
+                                NavigationLink (
+                                    destination: ContentView(),
+                                    label: {
+                                        HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                    })
+                                
+                                // MARK: Test Card
+                                
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                            }
+                            
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
+        
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
